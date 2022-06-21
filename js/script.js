602da4bbcd0af243e58e24f2887f74c57b24250c
fanil <wolffanil@mail.ru>
2022-06-21 22:02:50 +0400

    one

---
+++ js/script.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //tabs
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+    
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if(target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+
+       
+    });
+
+    //timer
+
+    const deadline = '2020-05-11';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor()
+    }
+          
+    function setClock()
+});
+
+// const now = new Date();
+
+// console.log(now.getFullYear());
+// console.log(now.getDay());
+// console.log(now.get);
+
+// let start = new Date();
+
+// for(let i = 0; i < 100000; i++) {
+//     let some = i ** 3;
+// }
+
+// let end = new Date();
+
+// alert(end - start);
+

040b3a5fc92ed783683560ea99ee0de741fc8905
wolffanil2 <wolffanil@mail.ru>
2022-07-05 18:44:56 +0700

    ggg

--- js/script.js
+++ js/script.js
@@ -111,8 +111,7 @@ setClock('.timer', deadline);
 //modal
 
     const btnOpen = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        btnClose = document.querySelector('[data-close]');
+        modal = document.querySelector('.modal');
 
     function openModal() {
         modal.classList.add('show');
@@ -131,10 +130,8 @@ setClock('.timer', deadline);
         document.body.style.overflow = '';
     }
 
-    btnClose.addEventListener('click', closeModal);
-
     modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
+        if (e.target === modal || e.target.getAttribute('data-close') == '') {
         closeModal();
         }
 
@@ -146,7 +143,7 @@ setClock('.timer', deadline);
         }
     });
 
-    // const modalTimerId = setTimeout(openModal, 5000);
+    const modalTimerId = setTimeout(openModal, 50000);
 
     function showModalByScroll() { //
         if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight -1) {
@@ -214,9 +211,92 @@ setClock('.timer', deadline);
         'big'
     ).render();
 
+    //forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'img/form/spinner.svg',
+        success: 'Спасибо! Скоро мы с вами свяжеSмся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postDate(item);
+    });
+
+    function postDate(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('img');
+            statusMessage.src = message.loading;
+            statusMessage.style.cssText =`
+                display:block;
+                margin:0 auto;
+            `;
+            form.insertAdjacentElement('afterend', statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            // request.setRequestHeader('Content-type', 'multipart/form-data');
+            request.setRequestHeader('Content-type', 'application/json');
+
+            const formData = new FormData(form);
+
+            const object = {};
+
+            formData.forEach(function(value, key) {
+                object[key] = value;
+            });
+
+            const json = JSON.stringify(object);
+
+            request.send(json);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    showThanksModal(message.success);
+                    form.reset();
+                    setTimeout(() => {
+                        statusMessage.remove();
+                    }, 2000);
+                } else {
+                    showThanksModal(message.failure);
+                }
+            });
+        });
+
+        function showThanksModal(message) {
+            const prevModalDialog = document.querySelector('.modal__dialog');
+
+            prevModalDialog.classList.add('hide');
+            openModal();
+
+            const thanksModal = document.createElement('div');
+            thanksModal.classList.add('modal__dialog');
+            thanksModal.innerHTML = `
+                <div class="modal__content">
+                    <div class="modal__close" data-close>×</div>
+                    <div class="modal__title">${message}</div>
+                </div>
+            `;
+
+            document.querySelector('.modal').append(thanksModal);
+            setTimeout(() => {
+                thanksModal.remove();
+                prevModalDialog.classList.add('show');
+                prevModalDialog.classList.remove('hide');
+                closeModal();
+            }, 4000);
+        }
+    }
 
 
-    // class Rect {
+
+    // class Rect {                        // со главной буквой называем класс
     //     constructor(name, age) {
     //         this.name = name;
     //         this.age = age;
@@ -267,4 +347,21 @@ setClock('.timer', deadline);
 // let end = new Date();
 
 // alert(end - start);
+
+// JSON
+
+// const app = {
+//     dd: 123,
+//     jg:'ann',
+//     parents: {
+//         mom:'gg',
+//         family:'ff'
+//     }
+// };
+
+// const clone = JSON.parse(JSON.stringify(app));
+
+// clone.parents.mom = 'hh';
+
+// console.log(clone);
 
